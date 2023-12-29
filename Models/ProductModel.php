e998140504b17d8864dfb81e7b3d9a4d9c5fa07e
Alan Eugenio <alan.349@hotmail.com>
2023-12-29 20:02:53 -0300

    Created function to retrive products paginated

--- Models/ProductModel.php
+++ Models/ProductModel.php
@@ -4,4 +4,13 @@ class ProductModel extends Model {
 
     protected $id;
 
+    public function selectPaginated(int $ini) {
+        $pstmt = $this->connection->prepare(
+            'SELECT P.id FROM product AS P 
+            LIMIT ' . $ini . ', ' . PAGINATION_QUANTITY
+        );
+        $pstmt->execute() or die(print_r($pstmt->errorInfo()));
+
+        return $pstmt->fetchAll(PDO::FETCH_CLASS, "ProductModel");
+    }
 }

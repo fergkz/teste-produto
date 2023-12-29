47a49d767883e2f6822f4a20eecad6e32d7828a6
Alan Eugenio <alan.349@hotmail.com>
2023-12-29 20:06:08 -0300

    Fixed selectPaginated DB Name misspell, Created function to count all products

--- Models/ProductModel.php
+++ Models/ProductModel.php
@@ -6,11 +6,18 @@ class ProductModel extends Model {
 
     public function selectPaginated(int $ini) {
         $pstmt = $this->connection->prepare(
-            'SELECT P.id FROM product AS P 
+            'SELECT P.id FROM products AS P 
             LIMIT ' . $ini . ', ' . PAGINATION_QUANTITY
         );
         $pstmt->execute() or die(print_r($pstmt->errorInfo()));
 
         return $pstmt->fetchAll(PDO::FETCH_CLASS, "ProductModel");
+    }
+
+    public function selectCountAll() {
+        $pstmt = $this->connection->prepare('SELECT count(P.id) FROM products AS P');
+        $pstmt->execute() or die(print_r($pstmt->errorInfo()));
+
+        return $pstmt->fetchColumn();
     }
 }

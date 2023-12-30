af46e0f80a962939001267f6f2dcac0b1d72d6dd
Alan Eugenio <alan.349@hotmail.com>
2023-12-29 21:04:07 -0300

    Created function in Product model to retrieve product by id

--- Models/ProductModel.php
+++ Models/ProductModel.php
@@ -3,11 +3,12 @@
 class ProductModel extends Model {
 
     protected $id;
+    protected $attributes;
+    protected $assets;
 
     public function selectPaginated(int $ini) {
         $pstmt = $this->connection->prepare(
-            'SELECT P.id FROM products AS P 
-            LIMIT ' . $ini . ', ' . PAGINATION_QUANTITY
+            'SELECT P.id FROM products AS P LIMIT ' . $ini . ', ' . PAGINATION_QUANTITY
         );
         $pstmt->execute() or die(print_r($pstmt->errorInfo()));
 
@@ -19,5 +20,17 @@ class ProductModel extends Model {
         $pstmt->execute() or die(print_r($pstmt->errorInfo()));
 
         return $pstmt->fetchColumn();
+    }
+
+    public function select() {
+        $pstmt = $this->connection->prepare('SELECT P.id FROM products AS P WHERE id = :id');
+        $pstmt->bindValue("id", $this->id);
+        $pstmt->execute() or die(print_r($pstmt->errorInfo()));
+
+        if (!$row = $pstmt->fetchObject("ProductModel")) {
+            return null;
+        }
+
+        return $row;
     }
 }

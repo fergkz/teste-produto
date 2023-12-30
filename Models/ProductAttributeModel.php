5bc2c00d3a04feb894986d20f6b1d2fbf51980a7
Alan Eugenio <alan.349@hotmail.com>
2023-12-29 21:03:29 -0300

    Created function in Product Asset and Product Attribute models to retrieve by product id

--- Models/ProductAttributeModel.php
+++ Models/ProductAttributeModel.php
@@ -6,4 +6,18 @@ class ProductAttributeModel extends Model {
     protected $product_id;
     protected $title;
     protected $value;
+
+    public function selectByProduct() {
+        $pstmt = $this->connection->prepare('SELECT PA.* FROM product_attributes AS PA WHERE product_id = :product_id');
+        $pstmt->bindValue("product_id", $this->product_id);
+        $pstmt->execute() or die(print_r($pstmt->errorInfo()));
+
+        $listAttributes = [];
+
+        while ($row = $pstmt->fetchObject("ProductAttributeModel")) {
+            $listAttributes[] = $row;
+        }
+
+        return $listAttributes;
+    }
 }

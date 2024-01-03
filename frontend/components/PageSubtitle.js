c7d5dd85d02671ae445f4fb1542db184ee02ebe4
Alan Eugenio <alan.349@hotmail.com>
2024-01-03 18:25:43 -0300

    Created reusable PageSubtitle component

---
+++ frontend/components/PageSubtitle.js
@@ -0,0 +1,7 @@
+export default function PageSubtitle({ children }) {
+  return (
+    <h2 className="w-full pb-2 text-lg font-semibold leading-tight text-gray-800">
+      {children}
+    </h2>
+  );
+}

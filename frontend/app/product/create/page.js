2f3b67ba1113385f35c80d83b5be5385bfb98514
Alan Eugenio <alan.349@hotmail.com>
2024-01-04 17:06:36 -0300

    Change submit handler to work with html required validation

--- frontend/app/product/create/page.js
+++ frontend/app/product/create/page.js
@@ -36,73 +36,79 @@ export default function ProductCreate() {
 
   const handleInputChange = (e, index) => {
     let newAttributes = [...attributes];
-
     newAttributes[index][e.target.name] = e.target.value;
     setAttributes(newAttributes);
   };
 
   const handleSubmit = (e) => {
-    const formData = new FormData();
-    attributes.forEach((attribute, index) => {
-      formData.append(`title[${index}]`, attribute.title);
-      formData.append(`value[${index}]`, attribute.value);
-    });
-    files.forEach((file, index) => {
-      formData.append(`file[${index}]`, file.file);
-    });
+    e.preventDefault();
+    if (e.target.checkValidity()) {
+      const formData = new FormData();
+      attributes.forEach((attribute, index) => {
+        formData.append(`title[${index}]`, attribute.title);
+        formData.append(`value[${index}]`, attribute.value);
+      });
+      files.forEach((file, index) => {
+        formData.append(`file[${index}]`, file.file);
+      });
 
-    axios
-      .post(`${process.env.backendUrl}/product/store`, formData)
-      .then(() => (window.location = "/product"));
+      axios
+        .post(`${process.env.backendUrl}/product/store`, formData)
+        .then(() => (window.location = "/product"));
+    }
   };
 
   return (
     <Section>
       <PageTitle>Cadastrar Produto</PageTitle>
       <PageSubtitle>Atributos:</PageSubtitle>
-      {attributes.map((attribute, index) => (
-        <Grid key={index} gridCols="grid-cols-3 xl:grid-cols-6">
-          <FloatingLabel
-            variant="standard"
-            label="Nome"
-            name="title"
-            value={attribute.title}
-            onChange={(e) => handleInputChange(e, index)}
+      <form onSubmit={(e) => handleSubmit(e)}>
+        {attributes.map((attribute, index) => (
+          <Grid key={index} gridCols="grid-cols-3 xl:grid-cols-6">
+            <FloatingLabel
+              variant="standard"
+              label="Nome"
+              name="title"
+              value={attribute.title}
+              onChange={(e) => handleInputChange(e, index)}
+              required
+            />
+            <FloatingLabel
+              variant="standard"
+              label="Valor"
+              name="value"
+              value={attribute.value}
+              onChange={(e) => handleInputChange(e, index)}
+              required
+            />
+            <Button
+              onClick={() => removeAttribute(index)}
+              className="mb-4"
+              color="failure">
+              Remover
+            </Button>
+          </Grid>
+        ))}
+        <Button onClick={() => addAttribute()} className="mb-4" color="success">
+          Adicionar
+        </Button>
+        <PageSubtitle>Arquivos:</PageSubtitle>
+        <div className={!isFilepondReady ? "hidden" : ""}>
+          <FilePond
+            allowMultiple
+            files={files}
+            onupdatefiles={setFiles}
+            imagePreviewHeight={150}
+            oninit={() => setIsFilepondReady(true)}
+            acceptedFileTypes={["image/png", "image/jpeg", "application/pdf"]}
+            labelIdle='Arraste e solte seus arquivos aqui ou <span class="filepond--label-action">Clique para buscar no dispositivo</span>'
           />
-          <FloatingLabel
-            variant="standard"
-            label="Valor"
-            name="value"
-            value={attribute.value}
-            onChange={(e) => handleInputChange(e, index)}
-          />
-          <Button
-            onClick={() => removeAttribute(index)}
-            className="mb-4"
-            color="failure">
-            Remover
-          </Button>
-        </Grid>
-      ))}
-      <Button onClick={() => addAttribute()} className="mb-4" color="success">
-        Adicionar
-      </Button>
-      <PageSubtitle>Arquivos:</PageSubtitle>
-      <div className={!isFilepondReady ? "hidden" : ""}>
-        <FilePond
-          allowMultiple
-          files={files}
-          onupdatefiles={setFiles}
-          imagePreviewHeight={150}
-          oninit={() => setIsFilepondReady(true)}
-          acceptedFileTypes={["image/png", "image/jpeg", "application/pdf"]}
-          labelIdle='Arraste e solte seus arquivos aqui ou <span class="filepond--label-action">Clique para buscar no dispositivo</span>'
-        />
-      </div>
-      {!isFilepondReady && <Spinner />}
-      <Button onClick={() => handleSubmit()} className="my-4" color="gray">
-        Cadastrar Produto
-      </Button>
+        </div>
+        {!isFilepondReady && <Spinner />}
+        <Button type="submit" className="my-4" color="gray">
+          Cadastrar Produto
+        </Button>
+      </form>
     </Section>
   );
 }

b98cac51da263a4287599c0c543b63d2af0ca419
Alan Eugenio <alan.349@hotmail.com>
2024-01-04 19:13:14 -0300

    Created function to delete Product Attribute model

--- backend/Models/ProductAttributeModel.php
+++ backend/Models/ProductAttributeModel.php
@@ -21,7 +21,6 @@ class ProductAttributeModel extends Model {
         return $listAttributes;
     }
 
-
     public function insert() {
         $pstmt = $this->connection->prepare('INSERT INTO product_attributes (product_id, title, `value`) VALUES (:product_id, :title, :value)');
         $pstmt->bindValue("product_id", $this->product_id);
@@ -30,5 +29,19 @@ class ProductAttributeModel extends Model {
         $pstmt->execute() or die(print_r($pstmt->errorInfo()));
 
         return $this->connection->lastInsertId();
+    }
+
+    public function update() {
+        $pstmt = $this->connection->prepare('UPDATE product_attributes SET title = :title, value = :value WHERE id = :id');
+        $pstmt->bindValue("id", $this->id);
+        $pstmt->bindValue("title", $this->title);
+        $pstmt->bindValue("value", $this->value);
+        $pstmt->execute() or die(print_r($pstmt->errorInfo()));
+    }
+
+    public function delete() {
+        $pstmt = $this->connection->prepare('DELETE FROM product_attributes WHERE id = :id');
+        $pstmt->bindValue("id", $this->id);
+        $pstmt->execute() or die(print_r($pstmt->errorInfo()));
     }
 }

24f308f4493fa04ec74a8df850236eec0f5ab696
Alan Eugenio <alan.349@hotmail.com>
2024-01-04 16:53:18 -0300

    Updated Product Create page to upload Product Assets

--- frontend/app/product/create/page.js
+++ frontend/app/product/create/page.js
@@ -5,12 +5,25 @@ import PageSubtitle from "@/components/PageSubtitle";
 import PageTitle from "@/components/PageTitle";
 import Section from "@/components/Section";
 import axios from "axios";
-import { Button, FloatingLabel } from "flowbite-react";
-import { redirect } from "next/navigation";
+import { Button, FloatingLabel, Spinner } from "flowbite-react";
 import { useState } from "react";
+
+import { FilePond, registerPlugin } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+registerPlugin(
+  FilePondPluginImageExifOrientation,
+  FilePondPluginImagePreview,
+  FilePondPluginFileValidateType
+);
 
 export default function ProductCreate() {
   const [attributes, setAttributes] = useState([]);
+  const [files, setFiles] = useState([]);
+  const [isFilepondReady, setIsFilepondReady] = useState(false);
 
   const addAttribute = () => {
     setAttributes([...attributes, { title: "", value: "" }]);
@@ -33,6 +46,9 @@ export default function ProductCreate() {
     attributes.forEach((attribute, index) => {
       formData.append(`title[${index}]`, attribute.title);
       formData.append(`value[${index}]`, attribute.value);
+    });
+    files.forEach((file, index) => {
+      formData.append(`file[${index}]`, file.file);
     });
 
     axios
@@ -72,6 +88,18 @@ export default function ProductCreate() {
         Adicionar
       </Button>
       <PageSubtitle>Arquivos:</PageSubtitle>
+      <div className={!isFilepondReady ? "hidden" : ""}>
+        <FilePond
+          allowMultiple
+          files={files}
+          onupdatefiles={setFiles}
+          imagePreviewHeight={150}
+          oninit={() => setIsFilepondReady(true)}
+          acceptedFileTypes={["image/png", "image/jpeg", "application/pdf"]}
+          labelIdle='Arraste e solte seus arquivos aqui ou <span class="filepond--label-action">Clique para buscar no dispositivo</span>'
+        />
+      </div>
+      {!isFilepondReady && <Spinner />}
       <Button onClick={() => handleSubmit()} className="my-4" color="gray">
         Cadastrar Produto
       </Button>
